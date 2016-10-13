f236849807589c3c4bb38a46721690a2b2b45436
ZaX <zacchi.paolo@gmail.com>
2016-10-13 14:45:41 +0200

    Primo test di inserimento

--- Program.cs
+++ Program.cs
@@ -20,6 +20,33 @@ namespace PostgresTestApplication
 
         public static void Main(string[] args)
         {
+            using (var db = new PostgreSqlContext())
+            {
+                db.ChangeTracker.AutoDetectChangesEnabled = false;
+                int righe = 100000;
+                for (int i = 0; i < righe; i++)
+                {
+                    db.Anagrafe.Add(new Anagrafe()
+                    {
+                        cod_fisc = Guid.NewGuid().ToString(),
+                        data_riferimento = DateTime.Now,
+                        data_nascita = new DateTime(2000, 1, 1),
+                        data_decesso = DateTime.Now,
+                        data_inizio_assistenza = DateTime.Now,
+                        data_fine_assistenza = DateTime.Now,
+                        sesso = "M",
+                        stato_id = "0",
+                        comune_nascita = "F205G",
+                        comune_residenza = "F205G",
+                        codice_medico = "MEDICO",
+                        asl_assistenza = "ASL001"
+                    });
+                }
+                var inizio = DateTime.Now;
+                db.SaveChanges();
+                var fine = DateTime.Now;
+                Console.WriteLine($"Inserite {righe} righe in {fine.Subtract(inizio).TotalSeconds} secondi.");
+            }
         }
     }
 }
